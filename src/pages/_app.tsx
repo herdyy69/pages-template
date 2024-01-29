76188edde6904a2205af99e7a1a33eea2a70b039
herdy3 <herdy@plabs.id>
2024-01-29 13:43:58 +0700

    Add font imports and apply fonts to main element

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Albert_Sans, Inter, Nunito } from "next/font/google";
+
+const nunito = Nunito({
+  subsets: ["latin"],
+  variable: "--nunito",
+  display: "swap",
+});
+
+const albertSans = Albert_Sans({
+  subsets: ["latin"],
+  variable: "--albert-sans",
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${nunito.className} ${albertSans.className}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,8 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { Albert_Sans, Inter, Nunito } from "next/font/google";
-
-const nunito = Nunito({
-  subsets: ["latin"],
-  variable: "--nunito",
-  display: "swap",
-});
-
-const albertSans = Albert_Sans({
-  subsets: ["latin"],
-  variable: "--albert-sans",
-  display: "swap",
-});
 
 export default function Document() {
   return (
-    <Html lang="en" className={`${nunito.className} ${albertSans.className}`}>
+    <Html lang="en">
       <Head />
       <body>
         <Main />
