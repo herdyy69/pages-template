3d884e99a19064741a5866dc4a99557aacb2851a
herdy3 <herdy@plabs.id>
2024-01-29 13:37:03 +0700

    Add font imports and apply font classes to HTML element

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,21 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Albert_Sans, Inter, Nunito } from "next/font/google";
+
+const nunito = Nunito({
+  subsets: ["latin"],
+  variable: "--nunito",
+  display: "swap",
+});
+
+const albertSans = Albert_Sans({
+  subsets: ["latin"],
+  variable: "--albert-sans",
+  display: "swap",
+});
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className={`${nunito.className} ${albertSans.className}`}>
       <Head />
       <body>
         <Main />
